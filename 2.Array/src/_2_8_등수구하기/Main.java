18ea0f12fb00582fcd4341899dd306f3857ebd2d
nakwon3345 <84373983+nakwon3345@users.noreply.github.com>
2021-12-06 15:14:46 +0900

    12/06

--- 2.Array/src/_2_8_등수구하기/Main.java
+++ 2.Array/src/_2_8_등수구하기/Main.java
@@ -3,7 +3,6 @@ package _2_8_등수구하기;
 import java.util.Scanner;
 
 public class Main {
-	// arr배열과 점수가 순서대로 정렬된 배열에서 번호를 매기고 그 점수를 arr로 변환(동일 등수일 때 불가?)?
 	public static int[] solution(int t, int[] arr){
 		int[] answer = new int[t];
 		for(int i = 0; i < t; i++) {
